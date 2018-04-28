2176771554d595a9847fcc7b2d656319f0a7dbb5
yong.tan <yong.tan@shuzun.net>
2018-04-28 18:06:11 +0800

    添加初始化sql脚本

---
+++ src/sql/init_table.sql
@@ -0,0 +1,29 @@
+
+DROP TABLE IF EXISTS `t_test`;
+CREATE TABLE `t_test` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '主键',
+  `alert_id` bigint(20) DEFAULT NULL COMMENT '业务ID',
+  `create_time` timestamp NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  `data` text COMMENT '数据',
+  `desc` varchar(500) DEFAULT NULL COMMENT '描述',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+INSERT INTO `alert`.`t_test` (`alert_id`, `create_time`, `data`, `desc`) VALUES ('1', '2018-04-13 10:00:00', '1', '1');
+INSERT INTO `alert`.`t_test` (`alert_id`, `create_time`, `data`, `desc`) VALUES ('2', '2018-04-13 10:10:00', '1', '1');
+
+DROP TABLE IF EXISTS `t_sum_talbe`;
+CREATE TABLE `t_sum_talbe` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '主键',
+  `parent_id` bigint(20) DEFAULT NULL COMMENT '业务ID',
+  `create_time` timestamp NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  `data` text COMMENT '数据',
+  `desc` varchar(500) DEFAULT NULL COMMENT '描述',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+INSERT INTO `alert`.`t_sum_talbe` (`parent_id`, `create_time`, `data`, `desc`) VALUES ('1', '2018-04-13 10:00:00', '1', '1');
+INSERT INTO `alert`.`t_sum_talbe` (`parent_id`, `create_time`, `data`, `desc`) VALUES ('1', '2018-04-13 10:01:00', '1', '1');
+
+INSERT INTO `alert`.`t_sum_talbe` (`parent_id`, `create_time`, `data`, `desc`) VALUES ('2', '2018-04-13 10:00:00', '1', '1');
+INSERT INTO `alert`.`t_sum_talbe` (`parent_id`, `create_time`, `data`, `desc`) VALUES ('2', '2018-04-13 10:01:00', '1', '1');
